ca03b5998bef00368829d867da2375874552f14f
Ali Ahmed Thawerani <athawerani@gmail.com>
2024-11-23 18:11:23 +0500

    First release

---
+++ app/src/main/java/com/example/assignment3d/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.assignment3d
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.example.assignment3d.databinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
